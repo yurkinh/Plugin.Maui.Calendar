18793b076132d9b8a0d525b8327dcf45d8905450
Yuriy Holembyovskyy <yholembyovskyy@gmail.com>
2024-01-03 12:12:30 +0200

    Removed swiper aware container; Fixed days view disposing

--- src/Plugin.Maui.Calendar/Shared/Controls/MonthDaysView.cs
+++ src/Plugin.Maui.Calendar/Shared/Controls/MonthDaysView.cs
@@ -13,6 +13,20 @@ namespace Plugin.Maui.Calendar.Controls;
 
 public partial class MonthDaysView : ContentView
 {
+    SwipeGestureRecognizer leftSwipeGesture;
+    SwipeGestureRecognizer rightSwipeGesture;
+    SwipeGestureRecognizer upSwipeGesture;
+    SwipeGestureRecognizer downSwipeGesture;
+
+    public static readonly BindableProperty SwipeDetectionDisabledProperty =
+      BindableProperty.Create(nameof(SwipeDetectionDisabled), typeof(bool), typeof(MonthDaysView), false);
+
+    public bool SwipeDetectionDisabled
+    {
+        get => (bool)GetValue(SwipeDetectionDisabledProperty);
+        set => SetValue(SwipeDetectionDisabledProperty, value);
+    }
+
     private Grid _daysControl;
 
     #region Bindable Properties
@@ -550,13 +564,62 @@ public partial class MonthDaysView : ContentView
 
     public MonthDaysView()
     {
+        if (!SwipeDetectionDisabled)
+        {
+            leftSwipeGesture = new() { Direction = SwipeDirection.Left };
+            rightSwipeGesture = new() { Direction = SwipeDirection.Right };
+            upSwipeGesture = new() { Direction = SwipeDirection.Up };
+            downSwipeGesture = new() { Direction = SwipeDirection.Down };
+
+            Loaded += LoadedMethod;
+            Unloaded += UnloadedMethod;
+        }
+
         RenderLayout();
     }
 
-    /// <summary>
-    /// Destructor for optimization
-    /// </summary>
-    ~MonthDaysView() => DiposeDayViews();
+    private void LoadedMethod(object sender, EventArgs e)
+    {
+        if (!SwipeDetectionDisabled)
+        {
+            GestureRecognizers.Add(leftSwipeGesture);
+            GestureRecognizers.Add(rightSwipeGesture);
+            GestureRecognizers.Add(upSwipeGesture);
+            GestureRecognizers.Add(downSwipeGesture);
+
+            leftSwipeGesture.Swiped += OnSwiped;
+            rightSwipeGesture.Swiped += OnSwiped;
+            upSwipeGesture.Swiped += OnSwiped;
+            downSwipeGesture.Swiped += OnSwiped;
+        }
+
+    }    
+
+    private void UnloadedMethod(object sender, EventArgs e)
+    {
+        if (!SwipeDetectionDisabled && GestureRecognizers.Count > 0)
+        {
+            GestureRecognizers.Remove(leftSwipeGesture);
+            GestureRecognizers.Remove(rightSwipeGesture);
+            GestureRecognizers.Remove(upSwipeGesture);
+            GestureRecognizers.Remove(downSwipeGesture);
+
+            leftSwipeGesture.Swiped -= OnSwiped;
+            rightSwipeGesture.Swiped -= OnSwiped;
+            upSwipeGesture.Swiped -= OnSwiped;
+            downSwipeGesture.Swiped -= OnSwiped;
+
+            leftSwipeGesture = null;
+            rightSwipeGesture = null;
+            upSwipeGesture = null;
+            downSwipeGesture = null;
+
+            Loaded -= LoadedMethod;
+            Unloaded -= UnloadedMethod;
+        }
+
+        DiposeDayViews();
+    }
 
     #region PropertyChanged
 
@@ -810,4 +873,33 @@ public partial class MonthDaysView : ContentView
         dayModel.EventIndicatorTextColor = eventIndicatorTextColor ?? EventIndicatorTextColor;
         dayModel.EventIndicatorSelectedTextColor = eventIndicatorSelectedTextColor ?? EventIndicatorSelectedTextColor;
     }
+
+    void OnSwiped(object sender, SwipedEventArgs e)
+    {
+        switch (e.Direction)
+        {
+            case SwipeDirection.Left:
+                OnSwipeLeft();
+                break;
+            case SwipeDirection.Right:
+                OnSwipeRight();
+                break;
+            case SwipeDirection.Up:
+                OnSwipeUp();
+                break;
+            case SwipeDirection.Down:
+                OnSwipeDown();
+                break;
+        }
+    }
+
+    public event EventHandler SwipedLeft;
+    public event EventHandler SwipedRight;
+    public event EventHandler SwipedUp;
+    public event EventHandler SwipedDown;
+
+    void OnSwipeLeft() => SwipedLeft?.Invoke(this, EventArgs.Empty);
+    void OnSwipeRight() => SwipedRight?.Invoke(this, EventArgs.Empty);
+    void OnSwipeUp() => SwipedUp?.Invoke(this, EventArgs.Empty);
+    void OnSwipeDown() => SwipedDown?.Invoke(this, EventArgs.Empty);
 }
