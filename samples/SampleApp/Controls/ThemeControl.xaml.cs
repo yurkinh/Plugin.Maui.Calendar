292874c91abed1c6ac29c037c41d805057940fc9
Mykhailo <davudenko.mukhailo@gmail.com>
2024-02-08 17:53:50 +0200

    Added Binding for theme properties

--- samples/SampleApp/Controls/ThemeControl.xaml.cs
+++ samples/SampleApp/Controls/ThemeControl.xaml.cs
@@ -6,4 +6,32 @@ public partial class ThemeControl : ContentView
 	{
 		InitializeComponent();
 	}
+	
+	public static readonly BindableProperty IsSystemThemeCheckedProperty = 
+		BindableProperty.Create(nameof(IsSystemThemeChecked), typeof(bool), typeof(ThemeControl), true);
+
+	public bool IsSystemThemeChecked
+	{
+		get => (bool)GetValue(IsSystemThemeCheckedProperty);
+		set => SetValue(IsSystemThemeCheckedProperty, value);
+	}
+
+
+	public static readonly BindableProperty IsDarkThemeCheckedProperty = 
+		BindableProperty.Create(nameof(IsDarkThemeChecked), typeof(bool), typeof(ThemeControl), false);
+
+	public bool IsDarkThemeChecked
+	{
+		get =>(bool)GetValue(IsDarkThemeCheckedProperty);
+		set =>SetValue(IsDarkThemeCheckedProperty, value);
+	}
+	
+	public static readonly BindableProperty IsLightThemeCheckedProperty = 
+		BindableProperty.Create(nameof(IsLightThemeChecked), typeof(bool), typeof(ThemeControl), false);
+
+	public bool IsLightThemeChecked
+	{
+		get =>(bool)GetValue(IsLightThemeCheckedProperty);
+		set =>SetValue(IsLightThemeCheckedProperty, value);
+	}
 }
