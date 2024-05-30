3398aea24d549750ee76455ecd92bde1f39f0dc4
Mykhailo <davudenko.mukhailo@gmail.com>
2024-05-30 17:57:14 +0300

    updates

---
+++ samples/SampleApp/Views/WindowsCalendarPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace SampleApp.Views;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class WindowsCalendarPage : ContentPage
+{
+	public WindowsCalendarPage()
+	{
+		InitializeComponent();
+	}
+}
