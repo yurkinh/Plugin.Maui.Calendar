30bb34ce0592a296faf5e9d06329ff3b076f2192
Mykhailo <davudenko.mukhailo@gmail.com>
2024-06-04 19:48:29 +0300

    added windows calendar sample

---
+++ samples/SampleApp/Controls/DefaultDeviceCalendarHeaders/WindowsCalendarHeader.xaml.cs
@@ -0,0 +1,10 @@
+namespace SampleApp.Controls;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class WindowsCalendarHeader : ContentView
+{
+	public WindowsCalendarHeader()
+	{
+		InitializeComponent();
+	}
+}
