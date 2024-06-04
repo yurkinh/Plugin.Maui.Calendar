0733da0493855e2f71aa0e8f783f2b79559ea9ed
Mykhailo <davudenko.mukhailo@gmail.com>
2024-06-04 21:15:36 +0300

    installed MauiCommunityToolkit

--- samples/SampleApp/MauiProgram.cs
+++ samples/SampleApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using Mopups.Hosting;
 
 namespace SampleApp;
@@ -11,7 +12,8 @@ public static class MauiProgram
 		builder
 			.UseMauiApp<App>()
 			.ConfigureMopups()
-			.ConfigureFonts(fonts =>
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
