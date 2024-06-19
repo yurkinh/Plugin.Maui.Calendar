b9ec10bfd1090e02185fe59bc3a80db52bb0d22e
Yuriy Holembyovskyy <yholembyovskyy@gmail.com>
2024-06-19 14:33:13 +0300

    Updated samples with Disposes method

--- samples/SampleApp/Views/MultiSelectionPage.xaml.cs
+++ samples/SampleApp/Views/MultiSelectionPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class MultiSelectionPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    void UnloadedHandler(object sender, EventArgs e)
+    {
+        calendar.Dispose();
+    }
 }
