4140c01c9791ceaa9a1290c9a83049318c495ec0
Yuriy Holembyovskyy <yholembyovskyy@gmail.com>
2025-02-17 18:54:29 +0200

    Nuget packages update and cleanup

--- samples/SampleApp/ViewModels/SimplePageViewModel.cs
+++ samples/SampleApp/ViewModels/SimplePageViewModel.cs
@@ -5,117 +5,88 @@ namespace SampleApp.ViewModels;
 
 public partial class SimplePageViewModel : BasePageViewModel
 {
-    public SimplePageViewModel() : base()
-    {
+	public SimplePageViewModel() : base()
+	{
 
-        // testing all kinds of adding events
-        // when initializing collection
-        var threeEventsTommorrow = GenerateEvents(3, "Simple3").ToArray();
-        Events = new EventCollection
-        {
-            [DateTime.Now.AddDays(-3)] = new List<EventModel>(GenerateEvents(10, "Cool")),
-            [DateTime.Now.AddDays(4)] = new List<EventModel>(GenerateEvents(2, "Simple2")),
-            [DateTime.Now.AddDays(2)] = new List<EventModel>(GenerateEvents(1, "Simple1")),
-            [DateTime.Now.AddDays(1)] = new DayEventCollection<EventModel>(threeEventsTommorrow) { Colors = threeEventsTommorrow.Select(e => e.Color).ToArray() },
-        };
+		// testing all kinds of adding events
+		// when initializing collection
+		var threeEventsTommorrow = GenerateEvents(3, "Simple3").ToArray();
+		Events = new EventCollection
+		{
+			[DateTime.Now.AddDays(-3)] = new List<EventModel>(GenerateEvents(10, "Cool")),
+			[DateTime.Now.AddDays(4)] = new List<EventModel>(GenerateEvents(2, "Simple2")),
+			[DateTime.Now.AddDays(2)] = new List<EventModel>(GenerateEvents(1, "Simple1")),
+			[DateTime.Now.AddDays(1)] = new DayEventCollection<EventModel>(threeEventsTommorrow) { Colors = threeEventsTommorrow.Select(e => e.Color).ToArray() },
+		};
 
-        // with add method
-        Events.Add(DateTime.Now.AddDays(-1), new List<EventModel>(GenerateEvents(5, "Cool")));
+		// with add method
+		Events.Add(DateTime.Now.AddDays(-1), new List<EventModel>(GenerateEvents(5, "Cool")));
 
-        // with indexer
-        Events[DateTime.Now] = new List<EventModel>(GenerateEvents(2, "Boring"));
+		// with indexer
+		Events[DateTime.Now] = new List<EventModel>(GenerateEvents(2, "Boring"));
 
+	}
 
-        /* Task.Delay(5000).ContinueWith(_ =>
-        {
-            // indexer - update later
-            Events[DateTime.Now] = new ObservableCollection<EventModel>(GenerateEvents(10, "Cool"));
+	static IEnumerable<EventModel> GenerateEvents(int count, string name)
+	{
+		return Enumerable.Range(1, count).Select(x => new EventModel
+		{
+			Name = $"{name} event{x}",
+			Description = $"This is {name} event{x}'s description!",
+			Color = Color.FromInt((int)(0xff000000 | Random.Shared.Next(0xffffff))),
+		});
+	}
 
-            // add later
-            Events.Add(DateTime.Now.AddDays(3), new List<EventModel>(GenerateEvents(5, "Cool")));
+	public EventCollection Events { get; }
 
-            // indexer later
-            Events[DateTime.Now.AddDays(10)] = new List<EventModel>(GenerateEvents(10, "Boring"));
+	[ObservableProperty]
+	int day = DateTime.Today.Day;
 
-            // add later
-            Events.Add(DateTime.Now.AddDays(15), new List<EventModel>(GenerateEvents(10, "Cool")));
+	[ObservableProperty]
+	int month = DateTime.Today.Month;
 
-            Month += 1;
+	[ObservableProperty]
+	int year = DateTime.Today.Year;
 
-            Task.Delay(3000).ContinueWith(t =>
-            {
-                // get observable collection later
-                var todayEvents = Events[DateTime.Now] as ObservableCollection<EventModel>;
+	[ObservableProperty]
+	DateTime? selectedDate = DateTime.Today;
 
-                // insert/add items to observable collection
-                todayEvents.Insert(0, new EventModel { Name = "Cool event insert", Description = "This is Cool event's description!" });
-                todayEvents.Add(new EventModel { Name = "Cool event add", Description = "This is Cool event's description!" });
+	[ObservableProperty]
+	DateTime minimumDate = new DateTime(2019, 4, 29);
 
-                Month += 1;
-            }, TaskScheduler.FromCurrentSynchronizationContext());
-        }, TaskScheduler.FromCurrentSynchronizationContext()); */
-    }
+	[ObservableProperty]
+	DateTime maximumDate = DateTime.Today.AddMonths(5);
 
-    static IEnumerable<EventModel> GenerateEvents(int count, string name)
-    {
-        return Enumerable.Range(1, count).Select(x => new EventModel
-        {
-            Name = $"{name} event{x}",
-            Description = $"This is {name} event{x}'s description!",
-            Color = Color.FromInt((int)(0xff000000 | Random.Shared.Next(0xffffff))),
-        });
-    }
+	[ObservableProperty]
+	string name;
 
-    public EventCollection Events { get; }
+	[ObservableProperty]
+	string description;
 
-    [ObservableProperty]
-    int day = DateTime.Today.Day;
+	[RelayCommand]
+	void Today()
+	{
+		Year = DateTime.Today.Year;
+		Month = DateTime.Today.Month;
+	}
+	[RelayCommand]
+	static async Task EventSelected(object item)
+	{
+		if (item is AdvancedEventModel eventModel)
+		{
+			var title = $"Selected: {eventModel.Name}";
+			var message = $"Starts: {eventModel.Starting:HH:mm}{Environment.NewLine}Details: {eventModel.Description}";
+			await Shell.Current.DisplayAlert(title, message, "Ok");
+		}
+	}
 
-    [ObservableProperty]
-    int month = DateTime.Today.Month;
+	[RelayCommand]
+	static async Task MonthChanged(MonthChangedEventArgs args)
+	{
+		string oldMonthName = new DateTime(1, args.OldMonth.Month, 1).ToString("MMMM");
+		string newMonthName = new DateTime(1, args.NewMonth.Month, 1).ToString("MMMM");
 
-    [ObservableProperty]
-    int year = DateTime.Today.Year;
-
-    [ObservableProperty]
-    DateTime? selectedDate = DateTime.Today;
-
-    [ObservableProperty]
-    DateTime minimumDate = new DateTime(2019, 4, 29);
-
-    [ObservableProperty]
-    DateTime maximumDate = DateTime.Today.AddMonths(5);
-
-    [ObservableProperty]
-    string name;
-
-    [ObservableProperty]
-    string description;
-
-    [RelayCommand]
-    void Today()
-    {
-        Year = DateTime.Today.Year;
-        Month = DateTime.Today.Month;
-    }
-    [RelayCommand]
-    static async Task EventSelected(object item)
-    {
-        if (item is AdvancedEventModel eventModel)
-        {
-            var title = $"Selected: {eventModel.Name}";
-            var message = $"Starts: {eventModel.Starting:HH:mm}{Environment.NewLine}Details: {eventModel.Description}";
-            await Shell.Current.DisplayAlert(title, message, "Ok");
-        }
-    }
-
-    [RelayCommand]
-    async Task MonthChanged(MonthChangedEventArgs args)
-    {
-        string oldMonthName = new DateTime(1, args.OldMonth.Month, 1).ToString("MMMM");
-        string newMonthName = new DateTime(1, args.NewMonth.Month, 1).ToString("MMMM");
-
-        string message = $"From {oldMonthName} to {newMonthName}";
-        await Shell.Current.DisplayAlert("Month Changed", message, "OK");
-    }
+		string message = $"From {oldMonthName} to {newMonthName}";
+		await Shell.Current.DisplayAlert("Month Changed", message, "OK");
+	}
 }
--- src/Plugin.Maui.Calendar/Shared/Models/DayModel.cs
+++ src/Plugin.Maui.Calendar/Shared/Models/DayModel.cs
@@ -7,247 +7,247 @@ using Plugin.Maui.Calendar.Styles;
 
 namespace Plugin.Maui.Calendar.Models;
 
-partial class DayModel : ObservableObject
+sealed partial class DayModel : ObservableObject
 {
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(BackgroundColor))]
-    [NotifyPropertyChangedFor(nameof(OutlineColor))]
-    DateTime date;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(BackgroundColor))]
+	[NotifyPropertyChangedFor(nameof(OutlineColor))]
+	DateTime date;
 
-    [ObservableProperty]
-    double dayViewSize;
+	[ObservableProperty]
+	double dayViewSize;
 
-    [TypeConverter(typeof(FontSizeConverter))]
-    [ObservableProperty]
-    double fontSize = 14;
+	[TypeConverter(typeof(FontSizeConverter))]
+	[ObservableProperty]
+	double fontSize = 14;
 
-    [ObservableProperty]
-    float dayViewCornerRadius;
+	[ObservableProperty]
+	float dayViewCornerRadius;
 
-    [ObservableProperty]
-    Style daysLabelStyle = DefaultStyles.DefaultLabelStyle;
+	[ObservableProperty]
+	Style daysLabelStyle = DefaultStyles.DefaultLabelStyle;
 
-    [ObservableProperty]
-    ICommand dayTappedCommand;
+	[ObservableProperty]
+	ICommand dayTappedCommand;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(
-        nameof(TextColor),
-        nameof(BackgroundColor),
-        nameof(IsEventDotVisible),
-        nameof(BackgroundEventIndicator),
-        nameof(BackgroundFullEventColor)
-    )]
-    bool hasEvents;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(
+		nameof(TextColor),
+		nameof(BackgroundColor),
+		nameof(IsEventDotVisible),
+		nameof(BackgroundEventIndicator),
+		nameof(BackgroundFullEventColor)
+	)]
+	bool hasEvents;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(TextColor), nameof(IsVisible))]
-    bool isThisMonth;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextColor), nameof(IsVisible))]
+	bool isThisMonth;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(
-        nameof(TextColor),
-        nameof(BackgroundColor),
-        nameof(OutlineColor),
-        nameof(EventColor),
-        nameof(BackgroundFullEventColor)
-    )]
-    bool isSelected;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(
+		nameof(TextColor),
+		nameof(BackgroundColor),
+		nameof(OutlineColor),
+		nameof(EventColor),
+		nameof(BackgroundFullEventColor)
+	)]
+	bool isSelected;
 
-    [ObservableProperty]
-    bool allowDeselect;
+	[ObservableProperty]
+	bool allowDeselect;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(IsVisible))]
-    bool otherMonthIsVisible;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(IsVisible))]
+	bool otherMonthIsVisible;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(TextColor))]
-    bool isDisabled;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextColor))]
+	bool isDisabled;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(TextColor))]
-    Color selectedTextColor = Colors.White;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextColor))]
+	Color selectedTextColor = Colors.White;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(TextColor))]
-    Color selectedTodayTextColor = Colors.Transparent;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextColor))]
+	Color selectedTodayTextColor = Colors.Transparent;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(TextColor))]
-    Color otherMonthColor = Colors.Silver;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextColor))]
+	Color otherMonthColor = Colors.Silver;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(TextColor))]
-    Color otherMonthSelectedColor = Colors.Gray;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextColor))]
+	Color otherMonthSelectedColor = Colors.Gray;
 
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(TextColor))]
-    Color weekendDayColor = Colors.Transparent;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextColor))]
+	Color weekendDayColor = Colors.Transparent;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(TextColor))]
-    Color deselectedTextColor = Colors.Transparent;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextColor))]
+	Color deselectedTextColor = Colors.Transparent;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(BackgroundColor))]
-    Color selectedBackgroundColor = Color.FromArgb("#2196F3");
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(BackgroundColor))]
+	Color selectedBackgroundColor = Color.FromArgb("#2196F3");
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(BackgroundColor))]
-    Color deselectedBackgroundColor = Colors.Transparent;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(BackgroundColor))]
+	Color deselectedBackgroundColor = Colors.Transparent;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(
-        nameof(IsEventDotVisible),
-        nameof(IsSecondEventDotVisible),
-        nameof(IsThirdEventDotVisible),
-        nameof(IsFourthEventDotVisible),
-        nameof(IsFifthEventDotVisible),
-        nameof(BackgroundEventIndicator),
-        nameof(BackgroundColor)
-    )]
-    EventIndicatorType eventIndicatorType = EventIndicatorType.BottomDot;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(
+		nameof(IsEventDotVisible),
+		nameof(IsSecondEventDotVisible),
+		nameof(IsThirdEventDotVisible),
+		nameof(IsFourthEventDotVisible),
+		nameof(IsFifthEventDotVisible),
+		nameof(BackgroundEventIndicator),
+		nameof(BackgroundColor)
+	)]
+	EventIndicatorType eventIndicatorType = EventIndicatorType.BottomDot;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(
-        nameof(EventColor),
-        nameof(BackgroundColor),
-        nameof(BackgroundFullEventColor)
-    )]
-    Color eventIndicatorColor = Color.FromArgb("#FF4081");
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(
+		nameof(EventColor),
+		nameof(BackgroundColor),
+		nameof(BackgroundFullEventColor)
+	)]
+	Color eventIndicatorColor = Color.FromArgb("#FF4081");
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(
-        nameof(IsSecondEventDotVisible),
-        nameof(IsThirdEventDotVisible),
-        nameof(IsFourthEventDotVisible),
-        nameof(IsFifthEventDotVisible),
-        nameof(SecondEventColor),
-        nameof(ThirdEventColor),
-        nameof(FourthEventColor),
-        nameof(FifthEventColor)
-    )]
-IReadOnlyList<Color> eventColors;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(
+		nameof(IsSecondEventDotVisible),
+		nameof(IsThirdEventDotVisible),
+		nameof(IsFourthEventDotVisible),
+		nameof(IsFifthEventDotVisible),
+		nameof(SecondEventColor),
+		nameof(ThirdEventColor),
+		nameof(FourthEventColor),
+		nameof(FifthEventColor)
+	)]
+	IReadOnlyList<Color> eventColors;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(
-        nameof(EventColor),
-        nameof(BackgroundColor),
-        nameof(BackgroundFullEventColor)
-    )]
-    Color eventIndicatorSelectedColor;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(
+		nameof(EventColor),
+		nameof(BackgroundColor),
+		nameof(BackgroundFullEventColor)
+	)]
+	Color eventIndicatorSelectedColor;
 
-    [ObservableProperty]
-    Color eventIndicatorTextColor;
+	[ObservableProperty]
+	Color eventIndicatorTextColor;
 
-    [ObservableProperty]
-    Color eventIndicatorSelectedTextColor;
+	[ObservableProperty]
+	Color eventIndicatorSelectedTextColor;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(OutlineColor))]
-    Color todayOutlineColor = Color.FromArgb("#FF4081");
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(OutlineColor))]
+	Color todayOutlineColor = Color.FromArgb("#FF4081");
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(TextColor))]
-    Color todayTextColor = Colors.Transparent;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(TextColor))]
+	Color todayTextColor = Colors.Transparent;
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(BackgroundColor))]
-    Color todayFillColor = Colors.Transparent;
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(BackgroundColor))]
+	Color todayFillColor = Colors.Transparent;
 
-    [ObservableProperty]
-    Color disabledColor = Color.FromArgb("#ECECEC");
+	[ObservableProperty]
+	Color disabledColor = Color.FromArgb("#ECECEC");
 
-    public bool IsEventDotVisible =>
-        HasEvents
-        && (
-            EventIndicatorType == EventIndicatorType.BottomDot
-            || EventIndicatorType == EventIndicatorType.TopDot
-        );
+	public bool IsEventDotVisible =>
+		HasEvents
+		&& (
+			EventIndicatorType == EventIndicatorType.BottomDot
+			|| EventIndicatorType == EventIndicatorType.TopDot
+		);
 
-    public bool IsSecondEventDotVisible => EventColors?.Count>=2 && (EventIndicatorType == EventIndicatorType.BottomDot || EventIndicatorType == EventIndicatorType.TopDot);
-    public bool IsThirdEventDotVisible => EventColors?.Count>=3 && (EventIndicatorType == EventIndicatorType.BottomDot || EventIndicatorType == EventIndicatorType.TopDot);
-    public bool IsFourthEventDotVisible => EventColors?.Count>=4 && (EventIndicatorType == EventIndicatorType.BottomDot || EventIndicatorType == EventIndicatorType.TopDot);
-    public bool IsFifthEventDotVisible => EventColors?.Count>=5 && (EventIndicatorType == EventIndicatorType.BottomDot || EventIndicatorType == EventIndicatorType.TopDot);
+	public bool IsSecondEventDotVisible => EventColors?.Count >= 2 && (EventIndicatorType == EventIndicatorType.BottomDot || EventIndicatorType == EventIndicatorType.TopDot);
+	public bool IsThirdEventDotVisible => EventColors?.Count >= 3 && (EventIndicatorType == EventIndicatorType.BottomDot || EventIndicatorType == EventIndicatorType.TopDot);
+	public bool IsFourthEventDotVisible => EventColors?.Count >= 4 && (EventIndicatorType == EventIndicatorType.BottomDot || EventIndicatorType == EventIndicatorType.TopDot);
+	public bool IsFifthEventDotVisible => EventColors?.Count >= 5 && (EventIndicatorType == EventIndicatorType.BottomDot || EventIndicatorType == EventIndicatorType.TopDot);
 
-    public FlexDirection EventLayoutDirection =>
-        (HasEvents && EventIndicatorType == EventIndicatorType.TopDot)
-            ? FlexDirection.ColumnReverse
-            : FlexDirection.Column;
+	public FlexDirection EventLayoutDirection =>
+		(HasEvents && EventIndicatorType == EventIndicatorType.TopDot)
+			? FlexDirection.ColumnReverse
+			: FlexDirection.Column;
 
-    public bool BackgroundEventIndicator =>
-        HasEvents && EventIndicatorType == EventIndicatorType.Background;
+	public bool BackgroundEventIndicator =>
+		HasEvents && EventIndicatorType == EventIndicatorType.Background;
 
-    public Color BackgroundFullEventColor =>
-        HasEvents && EventIndicatorType == EventIndicatorType.BackgroundFull
-            ? EventColor
-            : Colors.Transparent;
+	public Color BackgroundFullEventColor =>
+		HasEvents && EventIndicatorType == EventIndicatorType.BackgroundFull
+			? EventColor
+			: Colors.Transparent;
 
-    public Color EventColor => IsSelected ? EventIndicatorSelectedColor : EventIndicatorColor;
+	public Color EventColor => IsSelected ? EventIndicatorSelectedColor : EventIndicatorColor;
 
-    public Color SecondEventColor => EventColors != null && EventColors.Count >= 2 ? EventColors[ 1 ] : Colors.Transparent;
-    public Color ThirdEventColor => EventColors != null && EventColors.Count >= 3 ? EventColors[ 2 ] : Colors.Transparent;
-    public Color FourthEventColor => EventColors != null && EventColors.Count >= 4 ? EventColors[ 3 ] : Colors.Transparent;
-    public Color FifthEventColor => EventColors != null && EventColors.Count >= 5 ? EventColors[ 4 ] : Colors.Transparent;
+	public Color SecondEventColor => EventColors != null && EventColors.Count >= 2 ? EventColors[1] : Colors.Transparent;
+	public Color ThirdEventColor => EventColors != null && EventColors.Count >= 3 ? EventColors[2] : Colors.Transparent;
+	public Color FourthEventColor => EventColors != null && EventColors.Count >= 4 ? EventColors[3] : Colors.Transparent;
+	public Color FifthEventColor => EventColors != null && EventColors.Count >= 5 ? EventColors[4] : Colors.Transparent;
 
-    public Color OutlineColor => IsToday && !IsSelected ? TodayOutlineColor : Colors.Transparent;
+	public Color OutlineColor => IsToday && !IsSelected ? TodayOutlineColor : Colors.Transparent;
 
-    public Color BackgroundColor
-    {
-        get
-        {
-            if (!IsVisible || IsDisabled)
+	public Color BackgroundColor
+	{
+		get
+		{
+			if (!IsVisible || IsDisabled)
 			{
 				return DeselectedBackgroundColor;
 			}
 
 			return (BackgroundEventIndicator, IsSelected, IsToday) switch
-            {
-                (true, false, _) => EventIndicatorColor,
-                (true, true, _) => EventIndicatorSelectedColor,
-                (false, true, _) => SelectedBackgroundColor,
-                (false, false, true) => TodayFillColor,
-                (_, _, _) => DeselectedBackgroundColor
-            };
-        }
-    }
+			{
+				(true, false, _) => EventIndicatorColor,
+				(true, true, _) => EventIndicatorSelectedColor,
+				(false, true, _) => SelectedBackgroundColor,
+				(false, false, true) => TodayFillColor,
+				(_, _, _) => DeselectedBackgroundColor
+			};
+		}
+	}
 
-    public Color TextColor
-    {
-        get
-        {
-            if (!IsVisible)
+	public Color TextColor
+	{
+		get
+		{
+			if (!IsVisible)
 			{
 				return OtherMonthColor;
 			}
 
 			return (IsDisabled, IsSelected, HasEvents, IsThisMonth, IsToday, IsWeekend) switch
-            {
-                (true, _, _, _, _, _) => DisabledColor,
-                (false, true, false, true, true, _)
-                    => SelectedTodayTextColor == Colors.Transparent
-                        ? SelectedTextColor
-                        : SelectedTodayTextColor,
-                (false, true, false, true, false, _) => SelectedTextColor,
-                (false, true, true, true, _, _) => EventIndicatorSelectedTextColor,
-                (false, false, true, true, _, _) => EventIndicatorTextColor,
-                (false, false, _, false, _, _) => OtherMonthColor,
-                (false, true, _, false, _, _) => OtherMonthSelectedColor,
-                (false, false, false, true, true, _)
-                    => TodayTextColor == Colors.Transparent ? DeselectedTextColor : TodayTextColor,
-                (false, _, _, _, _, true) => WeekendDayColor,
-                (false, false, false, true, false, _) => DeselectedTextColor,
-            };
-        }
-    }
+			{
+				(true, _, _, _, _, _) => DisabledColor,
+				(false, true, false, true, true, _)
+					=> SelectedTodayTextColor == Colors.Transparent
+						? SelectedTextColor
+						: SelectedTodayTextColor,
+				(false, true, false, true, false, _) => SelectedTextColor,
+				(false, true, true, true, _, _) => EventIndicatorSelectedTextColor,
+				(false, false, true, true, _, _) => EventIndicatorTextColor,
+				(false, false, _, false, _, _) => OtherMonthColor,
+				(false, true, _, false, _, _) => OtherMonthSelectedColor,
+				(false, false, false, true, true, _)
+					=> TodayTextColor == Colors.Transparent ? DeselectedTextColor : TodayTextColor,
+				(false, _, _, _, _, true) => WeekendDayColor,
+				(false, false, false, true, false, _) => DeselectedTextColor,
+			};
+		}
+	}
 
-    public bool IsVisible => IsThisMonth || OtherMonthIsVisible;
+	public bool IsVisible => IsThisMonth || OtherMonthIsVisible;
 
-    bool IsToday => Date.Date == DateTime.Today;
+	bool IsToday => Date.Date == DateTime.Today;
 
-    public bool IsWeekend =>
-        (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
-        && WeekendDayColor != Colors.Transparent;
+	public bool IsWeekend =>
+		(Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+		&& WeekendDayColor != Colors.Transparent;
 }
