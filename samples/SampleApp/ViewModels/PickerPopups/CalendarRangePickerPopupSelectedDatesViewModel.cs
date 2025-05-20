701c0c574b7ae5941883755bcf455456f39580b2
Yuriy Holembyovskyy <yholembyovskyy@gmail.com>
2025-05-20 11:45:00 +0300

    Update calendar plugin to use ObservableCollection for SelectedDates; add breaking change notice in README

--- samples/SampleApp/ViewModels/PickerPopups/CalendarRangePickerPopupSelectedDatesViewModel.cs
+++ samples/SampleApp/ViewModels/PickerPopups/CalendarRangePickerPopupSelectedDatesViewModel.cs
@@ -1,57 +1,59 @@
-﻿using Mopups.Services;
+﻿using System.Collections.ObjectModel;
+using Mopups.Services;
 using Plugin.Maui.Calendar.Enums;
 
 namespace SampleApp.ViewModels;
 
 public partial class CalendarRangePickerPopupSelectedDatesViewModel : BasePageViewModel
 {
-    public CalendarRangePickerPopupSelectedDatesViewModel()
-    {
-        SelectedDates =
-        [
-            DateTime.Today,
-            DateTime.Today.AddDays(6),
-        ];
-    }
+	public CalendarRangePickerPopupSelectedDatesViewModel()
+	{
+		SelectedDates =
+		new ObservableCollection<DateTime>
+			{
+				DateTime.Today,
+				DateTime.Today.AddDays(6),
+			};
+	}
 
-    public event Action<CalendarRangePickerResult> Closed;
+	public event Action<CalendarRangePickerResult> Closed;
 
-    [ObservableProperty]
-    DateTime maximumDate = DateTime.Today.AddYears(1);
+	[ObservableProperty]
+	DateTime maximumDate = DateTime.Today.AddYears(1);
 
-    [ObservableProperty]
-    DateTime minimumDate = DateTime.Today.AddYears(-1);
+	[ObservableProperty]
+	DateTime minimumDate = DateTime.Today.AddYears(-1);
 
-    [ObservableProperty]
-    DateTime shownDate = DateTime.Today;
+	[ObservableProperty]
+	DateTime shownDate = DateTime.Today;
 
-    [ObservableProperty]
-    List<DateTime> selectedDates = null;
+	[ObservableProperty]
+	ObservableCollection<DateTime> selectedDates = null;
 
-    [ObservableProperty]
-    WeekLayout calendarLayout = WeekLayout.Month;
+	[ObservableProperty]
+	WeekLayout calendarLayout = WeekLayout.Month;
 
-    [RelayCommand]
-    async Task Cancel()
-    {
-        Closed?.Invoke(new CalendarRangePickerResult() { IsSuccess = false });
-        await MopupService.Instance.PopAsync();
-    }
+	[RelayCommand]
+	async Task Cancel()
+	{
+		Closed?.Invoke(new CalendarRangePickerResult() { IsSuccess = false });
+		await MopupService.Instance.PopAsync();
+	}
 
-    [RelayCommand]
-    void Clear()
-    {
-        SelectedDates = null;
-    }
+	[RelayCommand]
+	void Clear()
+	{
+		SelectedDates = null;
+	}
 
-    [RelayCommand]
-    async Task Success()
-    {
-        Closed?.Invoke(new CalendarRangePickerResult()
-        {
-            IsSuccess = true,
-            SelectedDates = SelectedDates
-        });
-        await MopupService.Instance.PopAsync();
-    }
+	[RelayCommand]
+	async Task Success()
+	{
+		Closed?.Invoke(new CalendarRangePickerResult()
+		{
+			IsSuccess = true,
+			SelectedDates = SelectedDates?.ToList()
+		});
+		await MopupService.Instance.PopAsync();
+	}
 }
