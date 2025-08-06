0cebcd9883ee28f9bf25c1b82216f834a7c5bfec
Mykhailo Davydenko <davudenko.mukhailo@outlook.com>
2025-08-06 12:03:56 +0300

    Fixed misspelling

---
+++ samples/SampleApp/Controls/CalendarEvent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+
+namespace SampleApp.Controls;
+
+public partial class CalendarEvent : ContentView
+{
+    public static readonly BindableProperty CalendarEventCommandProperty =
+        BindableProperty.Create(nameof(CalendarEventCommand), typeof(ICommand), typeof(CalendarEvent), null);
+
+    public CalendarEvent()
+    {
+        InitializeComponent();
+    }
+
+    public ICommand CalendarEventCommand
+    {
+        get => (ICommand)GetValue(CalendarEventCommandProperty);
+        set => SetValue(CalendarEventCommandProperty, value);
+    }
+
+    void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
+    {
+        if (BindingContext is AdvancedEventModel eventModel)
+		{
+			CalendarEventCommand?.Execute(eventModel);
+		}
+	}
+}
