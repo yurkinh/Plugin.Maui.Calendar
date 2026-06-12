208d4e5acf4f235a7ca3ab7e2b7ba7e9735e8b73
Yuriy Holembyovskyy <yholembyovskyy@gmail.com>
2026-06-12 16:57:13 +0300

    fix: update package references and font handling for Maui

--- samples/SampleApp/MauiProgram.cs
+++ samples/SampleApp/MauiProgram.cs
@@ -23,18 +23,11 @@ public static class MauiProgram
             .InjectServices()
             .InjectViewsAndViewModels()
             .ConfigureFonts(fonts =>
-            {
-#if ANDROID
+			{
 				fonts.AddFont("opensans-regular.ttf", "OpenSansRegular");
 				fonts.AddFont("opensans-semibold.ttf", "OpenSansSemibold");
 				fonts.AddFont("font-awesome-6-free-solid.otf", "FontAwesomeSolid");
 				fonts.AddFont("font-awesome-6-free-regular.otf", "FontAwesomeRegular");
-#else
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                fonts.AddFont("font-awesome-6-free-solid.otf", "FontAwesomeSolid");
-                fonts.AddFont("font-awesome-6-free-regular.otf", "FontAwesomeRegular");
-#endif
 			});
 
 
